7b88fc1e9b83b24ea00748305435648bf4946450
André Saxton <andre_saxton@hotmail.com>
2022-10-24 17:52:21 -0700

    Refazendo Biblioteca react-navigation e Criação da navegação em aba

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { AppRotas } from './src/rotas/AppRotas';
 
 import Home from './src/telas/Home'
 
 function App(){
   return <SafeAreaView style={estilos.tela}>
-     <Home />
+     {/* <Home /> */}
+     <AppRotas />
     </SafeAreaView>
 }
 
--- src/rotas/AppRotas.js
+++ src/rotas/AppRotas.js
@@ -1,17 +1,19 @@
 import React from "react";
-s
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Home from '../telas/Home';
 import MelhoresProdures from '../telas/MelhoresProdutores';
 
+import ProdutorRotas from "./ProdutorRotas";
+
 const  Tab = createBottomTabNavigator();
 
 export function AppRotas(){
     return <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home} />
+        <Tab.Screen name='Home' component={ProdutorRotas} />
         <Tab.Screen name='Melhores Produtores' component={MelhoresProdures} />
       </Tab.Navigator>
     </NavigationContainer>
